adff98602e12548a44c0068fb38bb9a0d26fb667
samerzmd <samerzmd@gmail.com>
2015-04-24 16:25:27 +0300

    Moving To Android Studio

--- gen/com/samerzmd/testthismap/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.samerzmd.testthismap;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/serverReqeusts/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author XZz_Yassin
- *
- */
-package serverReqeusts;
